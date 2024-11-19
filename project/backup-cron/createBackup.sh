b9df29387996fc149c75725bde1d3bcef8440949
tfhuhtal <tuomas.huhtala@icloud.com>
2024-11-19 10:19:24 +0200

    added service account

--- project/backup-cron/createBackup.sh
+++ project/backup-cron/createBackup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+
+
+# Echo the account being used by gsutil
+echo "The active account being used by gsutil is:"
+gcloud auth list
+
 BACKUP_PATH="/tmp/backup-$(date +\%Y-\%m-\%d).sql"
 
 PGPASSWORD=$POSTGRES_PASSWORD pg_dump --host=postgres-db-svc --username=postgres --file=$BACKUP_PATH --format=custom
 
-gsutil cp $BACKUP_PATH gs://project-db-backup 
+gsutil cp $BACKUP_PATH gs://postgres-backup-tfhuhtal
 
 rm $BACKUP_PATH
